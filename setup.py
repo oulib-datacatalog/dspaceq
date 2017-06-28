a2d892f43cc94e0d9e67f0da3090f8f846aa206d
Mark Stacy <mbstacy@gmail.com>
2017-06-28 12:43:55 -0500

    update setup version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='dspaceq',
-      version='0.0',
+      version='0.0.1',
       packages= find_packages(),
       package_data={'dspaceq':['tasks/templates/*.tmpl','dspaceq/tasks/templates/*.tmpl']},
       install_requires=[
