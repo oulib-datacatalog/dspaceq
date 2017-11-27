dd99ab7b6668a9a35afd5c9258f87533a05c76f1
Tyler Pearson <tdpearson@users.noreply.github.com>
2017-11-27 13:52:26 -0600

    tick version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='dspaceq',
-      version='0.0.22',
+      version='0.0.23',
       packages= find_packages(),
       package_data={'dspaceq':['tasks/templates/*.tmpl','dspaceq/tasks/templates/*.tmpl']},
       install_requires=[
