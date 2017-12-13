3dd4178ed2ac267ffb66ffcae890a71836ee5c19
tdpearson <tylerdpearson@gmail.com>
2017-12-13 13:33:31 -0600

    fix additional typos

--- dspaceq/tasks/tasks.py
+++ dspaceq/tasks/tasks.py
@@ -333,10 +333,10 @@ def remove_etd_catalog_record(id):
         return {"error": "etd collection is missing"}
 
     etd = db_client.catalog.etd
-    record = etd.find_one({'_id': ObjectID(id)})
+    record = etd.find_one({'_id': ObjectId(id)})
     if record:
-        #etd.remove({'_id': ObjectID(id)})
-        etd.delete_one({'_id': ObjectID(id)})  # limit to at most one record
+        #etd.remove({'_id': ObjectId(id)})
+        etd.delete_one({'_id': ObjectId(id)})  # limit to at most one record
         logging.info("Removed {0} from etd collection")
         return "Record {0} has been removed".format(id)
     else:
