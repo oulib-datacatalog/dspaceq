45a4396acb0a7f8e12a10ea868d11d6c69addaea
tdpearson <tylerdpearson@gmail.com>
2018-01-03 10:59:32 -0600

    prepend bucket to s3 path

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='dspaceq',
-      version='0.0.40',
+      version='0.0.41',
       packages= find_packages(),
       package_data={'dspaceq':['tasks/templates/*.tmpl','dspaceq/tasks/templates/*.tmpl','tasks/xslt/*']},
       install_requires=[
