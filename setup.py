8781be8c50055b06800624db19a1f6e4c8bad49f
tdpearson <tylerdpearson@gmail.com>
2018-03-06 12:39:09 -0600

    add missing requirement

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='dspaceq',
-      version='0.0.50',
+      version='0.0.51',
       packages= find_packages(),
       package_data={'dspaceq':['tasks/templates/*.tmpl','dspaceq/tasks/templates/*.tmpl','tasks/xslt/*']},
       install_requires=[
@@ -12,6 +12,7 @@ setup(name='dspaceq',
           'xlrd==1.0.0',
           'xlwt==1.0.0',
           'pandas',
+          'numpy',
           'jinja2',
           'boto3',
           'lxml',
