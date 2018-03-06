7da02978cbd0472daaed26bdc91693a977eec11d
tdpearson <tylerdpearson@gmail.com>
2018-03-06 12:46:43 -0600

    remove csience from master

--- dspaceq/tasks/__init__.py
+++ dspaceq/tasks/__init__.py
@@ -1,2 +1,1 @@
 from tasks import *
-from cscience import *
--- setup.py
+++ setup.py
@@ -2,17 +2,13 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='dspaceq',
-      version='0.0.51',
+      version='0.0.52',
       packages= find_packages(),
-      package_data={'dspaceq':['tasks/templates/*.tmpl','dspaceq/tasks/templates/*.tmpl','tasks/xslt/*']},
+      package_data={'dspaceq':['tasks/xslt/*']},
       install_requires=[
           'celery==3.1.22',
           'pymongo==3.2.1',
           'requests==2.9.1',
-          'xlrd==1.0.0',
-          'xlwt==1.0.0',
-          'pandas',
-          'numpy',
           'jinja2',
           'boto3',
           'lxml',
