7add66d57db32aff10d3ebfaf5390e336ba42d16
MP Malahy <mpmalahy@ou.edu>
2018-08-31 16:25:03 -0500

    tick version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='dspaceq',
-      version='0.0.69',
+      version='0.0.70',
       packages= find_packages(),
       package_data={'dspaceq':['tasks/templates/*.tmpl','dspaceq/tasks/templates/*.tmpl','tasks/xslt/*']},
       install_requires=[
