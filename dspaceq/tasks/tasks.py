8446a2089b3cfc454375947a609a1c5bed54d5df
Michael Malahy <mpmalahy@ou.edu>
2018-09-10 11:41:24 -0500

    changed tabs to four spaces in each location in tasks.py

--- dspaceq/tasks/tasks.py
+++ dspaceq/tasks/tasks.py
@@ -72,7 +72,7 @@ def bag_key(bag_details, collection, notify_email="libir@ou.edu"):
             filenames = [file.split("/")[-1] for file in bag_details[bag]["files"]]
             f.write("\n".join(filenames))
         with open(join(tempdir, "item_{0}".format(index), "dublin_core.xml"), "w") as f:
-	    print(bag_details[bag])
+        print(bag_details[bag])
             f.write(bag_details[bag]["metadata"].encode("utf-8"))
 
     try:
@@ -93,7 +93,7 @@ def bag_key(bag_details, collection, notify_email="libir@ou.edu"):
         #output = (["sudo", "-u", "tomcat", DSPACE_BINARY, "import", "-a", "-e", notify_email, "-c",
         #    collection, "-s", tempdir, "-m", '{0}/mapfile'.format(tempdir)])
       
-    	print(output)  
+        print(output)  
 
     except CalledProcessError as e:
         print("Failed to ingest: {0}".format(bag_details))
@@ -101,7 +101,7 @@ def bag_key(bag_details, collection, notify_email="libir@ou.edu"):
         return {"Error": "Failed to ingest: {0}".format(bag_details)}
         return e
         
-		
+        
    # finally:
         #rmtree(tempdir)
 
