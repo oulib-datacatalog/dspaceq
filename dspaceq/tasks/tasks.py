c569847e5c57d07a6155b216125a629e285baddc
Michael Malahy <mpmalahy@ou.edu>
2018-09-10 11:02:47 -0500

    put check_output in the correct position and changed indentation on 'return' statement

--- dspaceq/tasks/tasks.py
+++ dspaceq/tasks/tasks.py
@@ -79,8 +79,8 @@ def bag_key(bag_details, collection, notify_email="libir@ou.edu"):
         check_call(["chmod", "-R", "0775", tempdir])
         check_call(["chgrp", "-R", "tomcat", tempdir])
           
-        #check_call(["sudo", "-u", "tomcat", DSPACE_BINARY, "import", "-a", "-e", notify_email, "-c",
-        #collection, "-s", tempdir, "-m", '{0}/mapfile'.format(tempdir)])
+        output = check_output(["sudo", "-u", "tomcat", DSPACE_BINARY, "import", "-a", "-e", notify_email, "-c",
+            collection, "-s", tempdir, "-m", '{0}/mapfile'.format(tempdir)])
 
         with open('{0}/mapfile'.format(tempdir)) as f:
             results = []
@@ -90,8 +90,8 @@ def bag_key(bag_details, collection, notify_email="libir@ou.edu"):
                     results.append((item_match[item_index], handle))
        	    return {"Success": results}
 
-        output = (["sudo", "-u", "tomcat", DSPACE_BINARY, "import", "-a", "-e", notify_email, "-c",
-            collection, "-s", tempdir, "-m", '{0}/mapfile'.format(tempdir)])
+        #output = (["sudo", "-u", "tomcat", DSPACE_BINARY, "import", "-a", "-e", notify_email, "-c",
+        #    collection, "-s", tempdir, "-m", '{0}/mapfile'.format(tempdir)])
       
     	print(output)  
 
