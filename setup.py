4ad2dedda752a5dab4699a0fd05dbb595d743fc5
Tyler Pearson <tdpearson@users.noreply.github.com>
2018-11-14 11:03:40 -0600

    add db libraries and tick version

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='dspaceq',
-      version='0.0.61',
+      version='0.0.66',
       packages=find_packages(),
       package_data={'dspaceq':['tasks/xslt/*']},
       install_requires=[
           'celery==3.1.22',
           'pymongo==3.2.1',
           'requests==2.20.0',
+          'sqlalchemy==1.2.12',
+          'psycopg2==2.7.3.1',
           'jinja2',
           'boto3',
           'lxml',
