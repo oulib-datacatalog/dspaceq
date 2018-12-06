a46ba5c1d82d3c66b32419c4711c5230a8a96822
MichaelMalahy <mpmalahy@ou.edu>
2018-12-06 15:25:23 -0600

    added default value to get(method)

--- dspaceq/tasks/tasks.py
+++ dspaceq/tasks/tasks.py
@@ -232,10 +232,8 @@ def notify_dspace_etd_loaded(args):
     args:
        args: {"success": {bagname: url}
     """
-    if "success" in args:
-        ingested_items = args
-    else:
-        pass
+
+    ingested_items = args.get("success", default=none)
     if ingested_items:
         ingested_url_lookup = {get_mmsid(bag): url for bag, url in ingested_items.items()}
         mmsids_regex = "|".join([get_mmsid(bag) for bag in ingested_items.keys()])
