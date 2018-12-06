24324f0b66cbfc7012359680f5f43f61357d9344
MichaelMalahy <mpmalahy@ou.edu>
2018-12-06 14:56:27 -0600

    isolated etd notification function for debugging

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='dspaceq',
-      version='0.0.143',
+      version='0.0.144',
       packages= find_packages(),
       package_data={'dspaceq':['tasks/xslt/*']},
       install_requires=[
