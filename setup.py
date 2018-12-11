2abfa5d7106e53248c2a7ee97bca34ac497ce634
MichaelMalahy <mpmalahy@ou.edu>
2018-12-11 12:50:10 -0600

    restored rmtree

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='dspaceq',
-      version='0.0.166',
+      version='0.0.167',
       packages= find_packages(),
       package_data={'dspaceq':['tasks/xslt/*']},
       install_requires=[
