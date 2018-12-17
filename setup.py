a734d18dee3947c6370d193cb2d6a13d7f2bfcb1
MichaelMalahy <mpmalahy@ou.edu>
2018-12-17 10:52:20 -0600

    corrected indentation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='dspaceq',
-      version='0.0.17333',
+      version='0.0.174',
       packages= find_packages(),
       package_data={'dspaceq':['tasks/xslt/*']},
       install_requires=[
