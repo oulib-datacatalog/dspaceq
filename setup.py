3797b25e0cc956eebc93ecb637621caa0e95fe67
MichaelMalahy <mpmalahy@ou.edu>
2018-12-18 11:29:57 -0600

    removed duplicated 'with open' method

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='dspaceq',
-      version='0.0.174',
+      version='0.0.175',
       packages= find_packages(),
       package_data={'dspaceq':['tasks/xslt/*']},
       install_requires=[
