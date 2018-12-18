50b0bb54ba05bfe236a3f78a8c783ace24177c2c
MichaelMalahy <mpmalahy@ou.edu>
2018-12-18 16:07:55 -0600

    corrected scope of xml capture

--- dspaceq/tasks/tasks.py
+++ dspaceq/tasks/tasks.py
@@ -88,7 +88,8 @@ def dspace_ingest(bag_details, collection, notify_email="libir@ou.edu"):
                     with open(join(tempdir, "item_{0}".format(index), "{0}.xml".format(attribs)), "w") as f:
                         f.write(bag.values()[0]["metadata_{0}".format(attribs).keys]
 #                        print(bag.values()[0]["metadata_{0}"])
-
+                else:
+                    print("No additional metadata")
         else:
             print('The submitted item for bag ingest does not match format', bag)
             results.append(bag, "Failed to ingest-check submitted formatting")
