bc8ecc431311ecd025b9d426eaaadff78571dccd
Tyler Pearson <tdpearson@ou.edu>
2019-04-26 16:34:29 -0500

    troubleshooting

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='dspaceq',
-      version='0.0.209',
+      version='0.0.210',
       packages= find_packages(),
       package_data={'dspaceq':['tasks/xslt/*']},
       install_requires=[
