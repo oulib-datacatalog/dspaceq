38a0bae6da7d4754fcc830016830897cba4c5427
Tyler Pearson <tdpearson@ou.edu>
2019-04-26 15:15:12 -0500

    tick version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='dspaceq',
-      version='0.0.198',
+      version='0.0.199',
       packages= find_packages(),
       package_data={'dspaceq':['tasks/xslt/*']},
       install_requires=[
