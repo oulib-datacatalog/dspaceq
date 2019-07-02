9494dee461df0d27905933ccfb38ff6c17315ebf
MichaelMalahy <mpmalahy@ou.edu>
2019-07-02 14:11:40 -0500

    debugging readin text files for ETD metadata

--- dspaceq/tasks/tasks.py
+++ dspaceq/tasks/tasks.py
@@ -172,21 +172,24 @@ def ingest_thesis_dissertation(bag="", collection="",): #dspace_endpoint=REST_EN
             dc_xml_element.remove(result)
 
         for file in files:
+            if 'committee.txt' in file.lower():
              # If committee.txt is present, add contents to dc metadata
-            with open('committee.txt') as c:
-                c = etree.Element("dcvalue", element='contributor', qualifier='committeeMember')
-                c.text = committee_member
-                dc_xml_element.insert(0, c)
-                logging.info("Committee.txt added to metadata for: {0}".format(bag))
+                with open('committee.txt') as committee_file:
+                    member = committee_file.split("\n")
+                    c = etree.Element("dcvalue", element='contributor', qualifier='committeeMember')
+                    c.text = member
+                    dc_xml_element.insert(0, c)
+                    logging.info("Committee.txt added to metadata for: {0}".format(bag))
 
+            if 'abstract.txt' in file.lower():
             # If abstract.txt is present, add contents to dc metadata
-        with open('abstract.txt') as a:
-            a = etree.Element("dcvalue", element='contributor', qualifier='abstract')
-            a.text = abstract
-            dc_xml_element.insert(0, a)
-            logging.info("Abstract.txt added to metadata for: {0}".format(bag))
+                with open('abstract.txt') as abstract_file:
+                    a = etree.Element("dcvalue", element='contributor', qualifier='abstract')
+                    a.text = abstract_file
+                    dc_xml_element.insert(0, a)
+                    logging.info("Abstract.txt added to metadata for: {0}".format(bag))
 
-        dc = etree.tostring(dc_xml_element, pretty_print=True)
+        dc = etree.tostring(dc_xml_element)
 
         if collection == "":
             if type(bib_record) is not dict: #If this is a dictionary, we failed to get a valid bib_record
