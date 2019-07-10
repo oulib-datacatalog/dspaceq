c04d1e70eb065130c0fb4cfde84d5d40686b8c42
MichaelMalahy <mpmalahy@ou.edu>
2019-07-10 13:38:28 -0500

    debugging ETD metadata harvest obj dictionary keys

--- dspaceq/tasks/tasks.py
+++ dspaceq/tasks/tasks.py
@@ -189,7 +189,7 @@ def ingest_thesis_dissertation(bag="", collection="",): #dspace_endpoint=REST_EN
             # If abstract.txt is present, add contents to dc metadata
                 obj = s3.Object(s3_bucket, file)
                 
-                abstract = obj.get(file).read().decode('utf-8')
+                abstract = obj.get()['Abstract.txt'].read().decode('utf-8')
                 if abstract:
                     a = etree.Element("dcvalue", element='description', qualifier='abstract')
                     a.text = abstract
