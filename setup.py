0ac776291a5e031967fc481f4e802724e10f0deb
MichaelMalahy <mpmalahy@ou.edu>
2019-08-20 14:01:26 -0500

    updated version of SQLalchemy following prompting from github

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='dspaceq',
-      version='0.0.271',
+      version='0.0.272',
       packages= find_packages(),
       package_data={'dspaceq':['tasks/xslt/*']},
       install_requires=[
           'celery==3.1.22',
           'pymongo==3.2.1',
           'requests==2.20.0',
-          'sqlalchemy==1.2.12',
+          'sqlalchemy==1.3.7',
           'psycopg2==2.7.3.1',
           'jinja2',
           'boto3',
