1998db1155bdb05fc49121c95607a641179918f9
Tyler Pearson <tdpearson@users.noreply.github.com>
2020-08-05 09:59:32 -0500

    upgrade requests

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='dspaceq',
-      version='0.0.274',
+      version='0.0.275',
       packages= find_packages(),
       package_data={'dspaceq':['tasks/xslt/*']},
       install_requires=[
           'celery==3.1.22',
           'pymongo==3.2.1',
-          'requests==2.20.0',
+          'requests==2.24.0',
           'sqlalchemy==1.2.12',
           'psycopg2==2.7.3.1',
           'jinja2',
