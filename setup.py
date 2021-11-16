0a7eacb0d18f66b95fbb4b650a65af0bb4ed7aa1
Tyler Pearson <tdpearson@ou.edu>
2021-11-16 15:56:16 -0600

    verify shareok in bagname and use exact match of bagname to select record

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='dspaceq',      
-      version='0.1.0',
+      version='0.2.0',
       packages= find_packages(),
       package_data={'dspaceq':['tasks/xslt/*']},
       install_requires=[
