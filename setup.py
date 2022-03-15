50ebfad835f865e60174210e4cea46c7e4f5a009
Tyler Pearson <tdpearson@ou.edu>
2022-03-15 10:01:45 -0500

    update dependencies

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='dspaceq',      
-      version='0.1.0',
+      version='0.2.0',
       packages= find_packages(),
       package_data={'dspaceq':['tasks/xslt/*']},
       install_requires=[
           'celery',
           'pymongo',
           'requests==2.24.0',
-          'sqlalchemy==1.3.20',
-          'psycopg2==2.7.3.1',
+          'sqlalchemy==1.4.31',
+          'psycopg2==2.9.3',
           'jinja2',
           'boto3',
           'lxml',
